5865d566412d3cb0aba01c274cd8d8dd069eff3c
farizsalim <farizsalim10@yahoo.co.id>
2023-08-29 14:48:45 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import logo from './logo.gif';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        Welcome to Sigit Rendang Emulator
+      </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
